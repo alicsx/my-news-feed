b0b036c756da0242c1d359a321c695a7e9d74ee7
alicsx <17904920+alicsx@users.noreply.github.com>
2025-09-13 06:11:06 +0330

    Update gemini_trader.py

--- gemini_trader.py
+++ gemini_trader.py
@@ -20,7 +20,7 @@ if not TWELVEDATA_API_KEY:
 
 CURRENCY_PAIRS_TO_ANALYZE = [
     "EUR/USD", "GBP/USD", "USD/CHF", "EUR/JPY",
-    "AUD/JPY", "GBP/JPY", "EUR/AUD", "NZD/USD", "AUD/CAD","GBP/CAD","ETH/USD"
+    "AUD/JPY", "GBP/JPY", "EUR/AUD", "NZD/USD", "AUD/CAD","GBP/CAD","ETH/USD","AVX/USD"
 ]
 
 # --- ✨ تغییر ۲: تابع دریافت قیمت با Twelve Data جایگزین شد ✨ ---
